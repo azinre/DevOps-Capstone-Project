07be4e223b87aa88d449475e6eac22bdbe7d0175
azinre <azin.rg@gmail.com>
2025-09-16 13:18:19 -0400

    Add CI Build workflow.

--- README.md
+++ README.md
@@ -6,13 +6,8 @@ Capstone project for IBM DevOps &amp; Software Engineering. Builds a Customer Ac
 
 This repository contains the template to be used for the Final Project for the Coursera course **CI/CD Tools and Practices**.
 
-## Usage
 
-This repository is to be used as a template to create your own repository in your own GitHub account. No need to Fork it as it has been set up as a Template. This will avoid confusion when making Pull Requests in the future.
 
-From the GitHub **Code** page, press the green **Use this template** button to create your own repository from this template.
-
-Name your repo: `ci-cd-final-project`.
 
 ## Setup
 
--- service/__init__.py
+++ service/__init__.py
@@ -5,12 +5,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# This must be imported after the Flask app is created
-from service import routes               # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  # pylint: disable=wrong-import-position
+# Imports that must occur after app creation
+from service import routes  # noqa: F401,E402
+from service.common import log_handlers  # noqa: E402
 
+# Initialize logging
 log_handlers.init_logging(app, "gunicorn.error")
 
+# Startup logs
 app.logger.info(70 * "*")
 app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
